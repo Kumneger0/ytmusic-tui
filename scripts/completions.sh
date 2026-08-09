00124f91030f324327f4da451d44b2ac27e21a24
kumneger0 <kumnegerwondimu01@gmail.com>
2026-08-09 21:40:19 +0300

    fix: pass server urls to build scripts

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
+SERVER_URL="${1:-$SERVER_URL}"
 rm -rf completions
 mkdir completions
 for sh in bash zsh fish; do
-	go run main.go completion "$sh" >"completions/ytmusic-tui.$sh"
+	go run -ldflags "-X main.serverURL=${SERVER_URL}" main.go completion "$sh" >"completions/ytmusic-tui.$sh"
 done
