c8fe81637054381371ae9346f9b458733159621c
kumneger Wondimu <93655689+Kumneger0@users.noreply.github.com>
2026-08-09 22:06:15 +0300

    fix: pass server urls to build scripts (#159)

--- scripts/manpages.sh
+++ scripts/manpages.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+SERVER_URL="${1:-$SERVER_URL}"
 rm -rf manpages
 mkdir manpages
-go run . man | gzip -c -9 >manpages/ytmusic-tui.1.gz
+go run -ldflags "-X main.serverURL=${SERVER_URL}" . man | gzip -c -9 >manpages/ytmusic-tui.1.gz
